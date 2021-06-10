0980e4ecc30b0ca767871a9b26f1b0f3c8ac9cbf
vijejain <67664521+vijejain@users.noreply.github.com>
2021-06-10 11:17:57 -0700

    Update SampleJavaClass.java

--- src/main/java/com/amazon/samplelib/SampleJavaClass.java
+++ src/main/java/com/amazon/samplelib/SampleJavaClass.java
@@ -10,4 +10,21 @@ public class SampleJavaClass {
     public String sampleMethod() {
         return "sampleMethod() called!";
     }
+    
+    private void foo(String p) {
+       String s = "aloha";
+       System.out.println(String.format("This is a faulty message: %i", s));
+       System.out.format("No %s",1);
+       if ("1" == "2") {
+            //dosomething
+       }
+        
+       if ("2" == "4") {
+            //dosomething
+       }
+        
+       if ("3" == "4") {
+            //dosomething
+       }
+    }
 }
